38611bebcd1b1c827ccf8258cfc905b4554c4bf0
AudreyMeng <mengkejing0519@gmail.com>
2019-06-12 22:37:03 -0700

    answer&question

--- HU/config/routes.rb
+++ HU/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get '/about' =>  'home#about'
   post '/questions' => 'home#temp'
+  post '/answers' => 'home#temp'
+  get '/questions/:id' => 'home#question'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
