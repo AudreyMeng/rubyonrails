8bb471b8f7d2e194689f31c619052b6f3ac0c303
AudreyMeng <mengkejing0519@gmail.com>
2019-06-13 15:50:04 -0700

    new_question_form creates question in questions table

--- HU/app/controllers/questions_controller.rb
+++ HU/app/controllers/questions_controller.rb
@@ -1,2 +1,13 @@
 class QuestionsController < ApplicationController
+
+  def create
+    Question.create(question_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:email, :body)
+  end
 end
--- HU/config/routes.rb
+++ HU/config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   resources :questions
-  resources :models
   root 'home#index'
   get '/about' =>  'home#about'
-  post '/questions' => 'home#temp'
-  post '/answers' => 'home#temp'
-  get '/questions/:id' => 'home#question'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
