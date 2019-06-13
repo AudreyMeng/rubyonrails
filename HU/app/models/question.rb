e2f7372be893d0b4b730e736276e9b8d8689370e
AudreyMeng <mengkejing0519@gmail.com>
2019-06-13 12:25:33 -0700

    add SQLite with data

---
+++ HU/app/models/question.rb
@@ -0,0 +1,6 @@
+class Question < ApplicationRecord
+
+  def gravatar
+    "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}"
+  end
+end
--- HU/config/routes.rb
+++ HU/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :questions
+  resources :models
   root 'home#index'
   get '/about' =>  'home#about'
   post '/questions' => 'home#temp'
